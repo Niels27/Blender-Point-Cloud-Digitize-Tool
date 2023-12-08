0f940a3840375b937f8ed1d8c6d10b6bfe9773f1
Niels27 <nielsvdwal.27@gmail.com>
2023-12-08 11:57:39 +0100

    json import replaced with shapefile

--- webbased poc/app.py
+++ webbased poc/app.py
@@ -1,31 +1,24 @@
-from flask import Flask, send_from_directory, render_template, Response
-import os
+from flask import Flask, send_from_directory
+from flask_socketio import SocketIO
+from flask_cors import CORS  
+
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+socketio = SocketIO(app)
+
+# Define WebSocket events
+@socketio.on('message')
+def handle_message(data):
+    print('received message: ' + str(data))
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def index():
+    return send_from_directory('templates', 'index.html')
 
-@app.route('/get-point-cloud')
-def get_point_cloud():
-    
-    file_path = '/Users/niels/Documents/GitHub/Blender-Point-Cloud-Digitize-Tool/JSON files/auto.laz_points_colors_uncompressed.json'  
-
-    # Check if the file is gzip compressed (based on file extension)
-    if file_path.endswith('.gz'):
-        # Serve gzip compressed file
-        with open(file_path, 'rb') as file:
-            gzip_data = file.read()
-        response = Response(gzip_data)
-        response.headers['Content-Encoding'] = 'gzip'
-        response.headers['Content-Type'] = 'application/json'
-        return response
-    else:
-        # Serve regular JSON file
-        directory = os.path.dirname(file_path)
-        file_name = os.path.basename(file_path)
-        return send_from_directory(directory, file_name, as_attachment=True)
+@app.route('/shapefiles/<filename>')
+def serve_shapefile(filename):
+    return send_from_directory('shapefiles', filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
--- webbased poc/static/js/main.js
+++ webbased poc/static/js/main.js
@@ -1,126 +1,62 @@
-// Get the progress element
-const progressElement = document.getElementById('progress');
+// Initialize the WebSocket connection
+var socket = io.connect('http://' + document.domain + ':' + location.port);
 
-function fetchJSONWithProgress(url) {
-    return fetch(url).then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
+socket.on('connect', function() {
+    console.log('WebSocket connected!');
+});
 
-        const contentLength = +response.headers.get('Content-Length');
-        let receivedLength = 0;
-        let chunks = []; // Array to hold received text chunks
-
-        const reader = response.body.getReader();
-
-        function readNextChunk() {
-            return reader.read().then(({ done, value }) => {
-                if (done) {
-                    // All chunks have been read
-                    let json = chunks.join('');
-                    console.log("JSON start:", json.substring(0, 500));
-                    console.log("JSON end:", json.substring(json.length - 500));
-                    return JSON.parse(json);
-                }
-
-                // Convert chunk to text and accumulate
-                let chunkText = new TextDecoder("utf-8").decode(value);
-                chunks.push(chunkText);
-                receivedLength += value.length;
-                progressElement.textContent = `Received ${receivedLength} of ${contentLength}`;
-
-                // Read the next chunk
-                return readNextChunk();
-            });
-        }
-
-        return readNextChunk();
-    });
+// Function to send data to the server
+function sendDataToServer(data) {
+    socket.emit('message', data);
+    console.log("Data sent to server:", data);
 }
 
-function processPointCloud(pointCloudData) {
-    console.log("Data processed. Number of points:", pointCloudData.length);
-    console.log("Sample data:", pointCloudData.slice(0, 10));
-    progressElement.textContent = `Processing ${pointCloudData.length} points...`;
+// Initialize the map
+var map = L.map('map').setView([51.505, -0.09], 13); // Adjust with your desired coordinates
 
-    // Initialize Three.js scene
-    init(pointCloudData);
-}
+// Add OpenStreetMap tiles
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19
+}).addTo(map);
 
-fetch('/get-point-cloud')
-    .then(response => {
-        console.log("Received response, Content-Encoding:", response.headers.get("Content-Encoding"));
-        if (response.headers.get("Content-Encoding") === "gzip") {
-            // Handle gzipped data
-            return response.arrayBuffer().then(buffer => {
-                const decompressed = pako.inflate(buffer, { to: 'string' });
-                return JSON.parse(decompressed);
-            });
-        } else {
-            // Handle large JSON with progress
-            return fetchJSONWithProgress(response.url);
-        }
-    })
-    .then(processPointCloud)
-    .catch(error => {
-        console.error('Error processing data:', error);
-        progressElement.textContent = 'Error processing data.';
-    });
-function init(pointCloudData) {
-    // Scene, camera, and renderer setup
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+// Loading indicator element
+var loadingIndicator = document.createElement('div');
+loadingIndicator.innerText = 'Loading shapefile...';
+loadingIndicator.style.position = 'absolute';
+loadingIndicator.style.top = '10px';
+loadingIndicator.style.left = '10px';
+document.body.appendChild(loadingIndicator);
 
-    console.log("Initializing scene...");
-    progressElement.textContent = "Initializing scene...";
-
-    // Add points to the scene
-    const geometry = new THREE.BufferGeometry();
-    const vertices = [];
-    const colors = [];
+// Function to load and display the shapefile
+function loadShapefile(url) {
+    console.log("Starting to load shapefile:", url);
     
-    pointCloudData.forEach((point, index) => {
-        if (point.coords && point.color) {
-            // Handle the format with 'coords' and 'color' arrays
-            vertices.push(...point.coords);
-            colors.push(point.color[0] / 255, point.color[1] / 255, point.color[2] / 255);
-        } else if (!isNaN(point.x) && !isNaN(point.y) && !isNaN(point.z)) {
-            // Handle the format with 'x', 'y', 'z', and 'color' object
-            vertices.push(point.x, point.y, point.z);
-            colors.push(point.color.r / 255, point.color.g / 255, point.color.b / 255);
-        } else {
-            console.log(`Skipping invalid point at index ${index}`);
-        }
-
-        if (index % 10000 === 0) {
-            console.log(`Processing point ${index} / ${pointCloudData.length}`);
-            progressElement.textContent = `Processing point ${index} / ${pointCloudData.length}`;
-        }
+    shp(url).then(function(geojson) {
+        console.log("Shapefile loaded:", geojson);
+        var layer = L.geoJson(geojson).addTo(map);
+        map.fitBounds(layer.getBounds());
+        console.log("Shapefile successfully added to the map.");
+        loadingIndicator.remove(); // Remove loading indicator after loading
+    }).catch(function(error) {
+        console.error("Error loading shapefile:", error);
+        loadingIndicator.innerText = 'Failed to load shapefile.';
     });
-
-    geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
-    geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
-
-    const material = new THREE.PointsMaterial({ size: 0.05, vertexColors: true });
-    const points = new THREE.Points(geometry, material);
-    scene.add(points);
-
-    console.log("Points added to the scene.");
-    progressElement.textContent = "Points added to the scene.";
-
-    // Camera position
-    camera.position.z = 5;
-
-    console.log("Starting render loop...");
-    progressElement.textContent = "Starting render loop...";
-
-    // Animation loop
-    function animate() {
-        requestAnimationFrame(animate);
-        renderer.render(scene, camera);
-    }
-    animate();
 }
+
+// Load and display the shapefile
+loadShapefile("http://localhost:5000/shapefiles/shapefile.zip");
+
+// Handle map click events
+map.on('click', function(e) {
+    var latlng = e.latlng;
+    console.log("Clicked at latitude: " + latlng.lat + " and longitude: " + latlng.lng);
+
+    // Prepare the data to be sent
+    var data = {
+        latitude: latlng.lat,
+        longitude: latlng.lng
+    };
+
+    // Send the data to the server
+    sendDataToServer(data);
+});
